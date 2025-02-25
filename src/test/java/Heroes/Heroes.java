652f8d5b8c1c42d5d495ed5794444c98844d0e0b
Kratos-25 <josephkosalan01@gmail.com>
2025-02-25 14:02:58 +0530

    message

---
+++ src/test/java/Heroes/Heroes.java
@@ -0,0 +1,26 @@
+package Heroes;
+
+public class Heroes {
+
+	
+	public void spartacus() {
+		
+}
+	
+	public void kratos() {
+
+	}
+	
+	public void wolverin() {
+
+	}
+	
+	
+	public void jonsnow() {
+
+	}
+	
+	public void Ragnar() {
+
+	}
+}
